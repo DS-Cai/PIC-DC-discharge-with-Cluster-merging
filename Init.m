4cf5afe4b6aaf73d3970085cc4521cd9a5da9531
DS <124227014+DS-Cai@users.noreply.github.com>
2024-09-13 19:22:44 +0800

    Update Init.m

--- Init.m
+++ Init.m
@@ -1,12 +1,3 @@
-%% argc==1,initialize
-% file = fopen('picdata.bin', 'rb');
-% if file ~= -1
-%     fclose(file);
-%     disp('eduPIC: Warning: Data from previous calculation are detected.');
-%     disp('To start a new simulation from the beginning, please delete all output files before running ./eduPIC 0');
-%     disp('To continue the existing calculation, please specify the number of cycles to run, e.g. ./eduPIC 100');
-%     error(' Data from previous calculation are detected.')
-% end
 %% 初始化
 N_e = N_INIT;
 N_i =  N_INIT;
